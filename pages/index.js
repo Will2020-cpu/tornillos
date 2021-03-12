30a0b99cef4b978a881347c73e6f3f906c8d5665
madara <wcamacho221@gmail.com>
2021-03-12 03:56:34 -0500

    Seccion tornillos agregados

--- pages/index.js
+++ pages/index.js
@@ -3,31 +3,32 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Layout from '../components/NavBar/Layout'
 import { motion } from 'framer-motion'
+import { tornillos } from '../productos'
 
 
 const easing = [0.6, -0.05, 0.01, 0.99]
 
 
-const fadeInUp ={
-  initial:{
-    y:60,
-    opacity:0,
+const fadeInUp = {
+  initial: {
+    y: 60,
+    opacity: 0,
 
   },
-  animate:{
-    y:0,
-    opacity:1,
-    transition:{
-      duration:0.6,
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
       ease: easing
     }
   }
 }
 
-const stagger ={
-  animate:{
-    transition:{
-      staggerChildren:0.2
+const stagger = {
+  animate: {
+    transition: {
+      staggerChildren: 0.2
     }
   }
 }
@@ -35,23 +36,40 @@ const stagger ={
 
 export default function Home() {
   return (
-    <div >
+    <motion.div
+      exit={{ opacity: 0 }}
+      initial='initial'
+      animate='animate'
+    >
       <Head>
         <title>Zuky Tornillos</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
         <div className={styles.hero}>
-          <div className={styles.text}>
+          <motion.div variants={fadeInUp} className={styles.text}>
             <h1>Gama completa de tuercas, Tornillos y pernos</h1>
             <Link href='/'><a className="py-2  bg-customRed rounded-full px-3 text-white">Pedir al por mayor</a></Link>
-          </div>
+          </motion.div>
         </div>
         <div className="mt-10">
-         <h1 className="text-customRed uppercase text-2xl text-center">Tornillos</h1>
-
+          <h1 className="text-customRed uppercase text-2xl text-center">Tornillos</h1>
+            <div className="mt-10 w-11/12 m-auto grid grid-cols-6 gap-4">
+                {
+                  tornillos.map(({ nombre,imagen },i) =>(
+                    <div className="flex flex-col items-center justify-center space-y-2" key={i}>
+                        <div>
+                            <img src={imagen} alt={nombre} className="h-44"/>
+                        </div> 
+                        <div className="">
+                            <h1 className={styles.textImage}>{nombre}</h1>
+                        </div>
+                    </div>
+                  ))
+                }
+            </div>
         </div>
       </Layout>
-    </div>
+    </motion.div>
   )
 }
---
+++ productos.js
@@ -0,0 +1,26 @@
+export const tornillos = [
+    {
+        nombre:'Tornillo Casquete',
+        imagen:'/tornillo1.webp'
+    },
+    {
+        nombre:'Tornillo Hexagonal',
+        imagen:'/tornillo2.webp'
+    },
+    {
+        nombre: 'Tornillo Yeso',
+        imagen:'/tornillo3.webp'
+    },
+    {
+        nombre:'Tornillo Enchufe',
+        imagen:'/tornillo4.webp'
+    },
+    {
+        nombre:'Tornillo Chapa',
+        imagen: '/tornillo5.webp'
+    },
+    {
+        nombre: 'Tornillo Aglomerado',
+        imagen: '/tornillo6.webp'
+    }
+]
