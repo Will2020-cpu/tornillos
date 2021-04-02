bf6be43aa0e668419dff656dd2a2b724436ddd54
madara <wcamacho221@gmail.com>
2021-04-02 02:42:27 -0500

    Menu responsive agregado

--- components/NavBar/Sidebar.jsx
+++ components/NavBar/Sidebar.jsx
@@ -10,7 +10,7 @@ import {
 import React, { Fragment, useState } from 'react'
 import {makeStyles} from '@material-ui/core/styles';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faHome } from '@fortawesome/free-solid-svg-icons'
+import { faAddressCard, faBars, faHome, faIdCardAlt, faList, faPhotoVideo, faQuestion, faTools } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
 
 
@@ -48,8 +48,37 @@ return (
                         <Link  href="/"><ListItemText primary="Inicio"/></Link>
                     </ListItem>
 
-                    <ListItem button>
-                       <Link onClick={()=>setOpen(!open)} href="/"><a>Xd</a></Link>
+                    <ListItem button onClick={()=>setOpen(!open)}>
+                       <ListItemIcon><FontAwesomeIcon icon={faList}/></ListItemIcon>
+                       <Link href="/#categorias"><ListItemText primary="Categorias"/></Link>
+                    </ListItem>
+
+                    <ListItem button onClick={()=>setOpen(!open)}>
+                       <ListItemIcon><FontAwesomeIcon icon={faTools}/></ListItemIcon>
+                       <Link href="/#productos"><ListItemText primary="Productos"/></Link>
+                    </ListItem>
+
+                    <ListItem button onClick={()=>setOpen(!open)}>
+                       <ListItemIcon><FontAwesomeIcon icon={faQuestion}/></ListItemIcon>
+                       <Link href="/#whyus"><ListItemText primary="Porque nosotros"/></Link>
+                    </ListItem>
+
+
+                    <ListItem button onClick={()=>setOpen(!open)}>
+                       <ListItemIcon><FontAwesomeIcon icon={faAddressCard}/></ListItemIcon>
+                       <Link href="/#nosotros"><ListItemText primary="Sobre nosotros"/></Link>
+                    </ListItem>
+
+
+                    <ListItem button onClick={()=>setOpen(!open)}>
+                       <ListItemIcon><FontAwesomeIcon icon={faPhotoVideo}/></ListItemIcon>
+                       <Link href="/#galeria"><ListItemText primary="Galeria"/></Link>
+                    </ListItem>
+
+
+                    <ListItem button onClick={()=>setOpen(!open)}>
+                       <ListItemIcon><FontAwesomeIcon icon={faIdCardAlt}/></ListItemIcon>
+                       <Link href="/#contacto"><ListItemText primary="Contacto"/></Link>
                     </ListItem>
                 </List>
             </div>
